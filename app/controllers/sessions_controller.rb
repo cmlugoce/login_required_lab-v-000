ae7946a310977da9cc5c26c3a0bf5e9abe8b06ac
Cristina Lugo <cmlugoce@syr.edu>
2018-08-01 14:53:00 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
 
 
+def create
 
+ if params[:name] || params[:name].empty?
+   session[:name] = params[:name]
+   redirect_to controller: 'application', action: 'hello'
+
+ else
+   redirect_to controller: 'sessions', action: 'new'
+
+end 
 
 
 end 
